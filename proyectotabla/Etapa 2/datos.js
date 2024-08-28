0a95e0720601b3d7346f3a3c4fe203885a36d2e2
emaquintana <emanuelquintana083@gmail.com>
2024-08-28 16:48:59 -0300

    Etapa 2 terminada

---
+++ proyectotabla/Etapa 2/datos.js
@@ -0,0 +1,42 @@
+let Productos =[{
+    id :10,
+    nombre :"mayonesa",
+    descripcion: "natura",
+    precio: "USD 3",
+    cantidad: 20
+},{
+    id: 20,
+    nombre : "azucar",
+    descripcion: "Ledesma",
+    precio: "USD 50",
+    cantidad: 25
+},{
+    id: 30,
+    nombre : "arroz",
+    descripcion: "Tres gallos",
+    precio: "USD 30",
+    cantidad: 25
+
+},{
+    id: 40,
+    nombre : "fideos",
+    descripcion: "Mostachol",
+    precio: "USD 55",
+    cantidad: 25
+},{
+    id: 50,
+    nombre : "Jugo",
+    descripcion: "Baggio",
+    precio: "USD 10",
+    cantidad: 30
+},{
+    id: 60,
+    nombre : "jamon",
+    descripcion: "Calchaqui",
+    precio: "USD 400",
+    cantidad: 2
+}
+
+]
+
+export default Productos;
---
+++ proyectotabla/Etapa 2/main.js
@@ -0,0 +1,4 @@
+import products from "./datos.js"
+
+console.log(products);
+
